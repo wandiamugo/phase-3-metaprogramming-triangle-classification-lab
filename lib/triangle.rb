b384202db1cf7a909f90d542fb2c6dc85963629b
wandiamugo <wwandia173@gmail.com>
2022-08-20 10:48:53 +0300

    commit tree 342e8024e595b5c409aee81655ee4c4f5a4609ba

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
   # write code here
+  attr_accessor :size1, :size2, :size3
+
+  def initialize(size1, size2, size3)
+    @size1 = size1
+    @size2 = size2
+    @size3 = size3
+  end
+
+  def kind
+    triangle = [@size1, @size2, @size3].uniq
+    p triangle
+    if @size1 <= 0 || @size2 <= 0 || @size3 <= 0
+      raise TriangleError
+    end
+    if @size1 + @size2 <= @size3 || @size1 + @size3 <= @size2 || @size2 + @size3 <= @size1
+      raise TriangleError
+    end
+    if triangle.size == 1
+      :equilateral
+    elsif triangle.size == 2
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
